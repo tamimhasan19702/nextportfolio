210f65f3cc948ed64b1d14d1df99c954c93ed67a
tamimhasan197020 <tamimhasancu@gmail.com>
2024-05-26 19:02:00 +0600

    update globals.css, brainSvg.jsx and transitionProvider.jsx

--- src/components/transitionProvider.jsx
+++ src/components/transitionProvider.jsx
@@ -9,7 +9,9 @@ const TransitionProvider = ({ children }) => {
   const pathName = usePathname();
   return (
     <AnimatePresence mode="wait">
-      <div key={pathName} className="w-screen h-screen ">
+      <div
+        key={pathName}
+        className="w-screen h-screen overflow-y-scroll no-scrollbar">
         <motion.div
           className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40"
           animate={{ height: "0vh" }}
